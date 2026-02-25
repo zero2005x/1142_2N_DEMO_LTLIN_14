fb1b2beb848f2795fde2529b69302ab7e9b46ea3
zero2005x <zero2005x@gmail.com>
2026-02-25 20:20:39 +0800

    Refactor code structure for improved readability and maintainability

--- w01_typescript_14/src/tutorials/t1.js
+++ w01_typescript_14/src/tutorials/t1.js
@@ -46,11 +46,89 @@ var car = { brand: 'toyota', year: 2020 };
 car.brand = 'ford';
 // car.color = 'blue';
 var car1 = { brand: 'audi', year: 2021 };
-var car2 = { brand: 'audi' };
-console.log(car, car1);
+//let car2: { brand: string; year: number } = { brand: 'audi' };
+console.log(car1);
 var book = { title: 'book', cost: 20 };
 var pen = { title: 'pen', cost: 5 };
 var notebook = { title: 'notebook' };
 var items = [book, pen, notebook];
-items[0].title = 'new book'; // Error: Cannot assign to 'title' because it is a read-only proper
+// items[0].title = 'new book'; // Error: Cannot assign to 'title' because it is a read-only proper
 console.log(items);
+/* */
+function sayHi(name) {
+    console.log("Hello there ".concat(name.toUpperCase(), "!!!"));
+}
+sayHi('john');
+// sayHi(3)
+// sayHi('peter', 'random');
+function calculateDiscount(price) {
+    // price.toUpperCase();
+    var hasDiscount = true;
+    if (hasDiscount) {
+        return price;
+        // return 'Discount Applied';
+    }
+    return price * 0.9;
+}
+var finalPrice = calculateDiscount(200);
+console.log(finalPrice);
+// "any" example
+function addThree(number) {
+    var anotherNumber = 3;
+    return number + anotherNumber;
+}
+var addThreeResult = addThree(2);
+var someValue = addThreeResult;
+console.log(someValue);
+// run time error
+//someValue.myMethod();
+/* */
+// Function = Optional and Default Parameters
+function calculatePrice(price, discount) {
+    return price - (discount || 0);
+}
+var priceAfterDiscount = calculatePrice(100, 20);
+console.log(priceAfterDiscount); // Output: 80
+var priceWithoutDiscount = calculatePrice(300);
+console.log(priceWithoutDiscount); // Output: 300
+function calculateScore(initialScore, penaltyPoints) {
+    if (penaltyPoints === void 0) { penaltyPoints = 0; }
+    return initialScore - penaltyPoints;
+}
+var scoreAfterPenalty = calculateScore(100, 20);
+console.log(scoreAfterPenalty); // Output: 80
+var scoreWithoutPenalty = calculateScore(300);
+console.log(scoreWithoutPenalty); // Output: 300
+/* */
+function sum(message) {
+    var numbers = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        numbers[_i - 1] = arguments[_i];
+    }
+    var doubled = numbers.map(function (num) { return num * 2; });
+    console.log(doubled);
+    var total = numbers.reduce(function (previous, current) {
+        return previous + current;
+    }, 0);
+    return "".concat(message, " ").concat(total);
+}
+var sumResult = sum('The total is:', 1, 2, 3, 4, 5); // sumResult will be "The total is: 15"
+console.log(sumResult);
+/* */
+// Functions - Using Object as Function
+function createEmployee(_a) {
+    var id = _a.id;
+    return { id: id, isActive: id % 2 === 0 };
+}
+var first = createEmployee({ id: 1 });
+var second = createEmployee({ id: 2 });
+console.log(first, second);
+// alternative
+function createStudent(student) {
+    console.log("Welcome to the course ".concat(student.name.toUpperCase(), "!!!"));
+}
+var newStudent = {
+    id: 5,
+    name: 'anna',
+};
+createStudent(newStudent);
--- w01_typescript_14/src/tutorials/t1.ts
+++ w01_typescript_14/src/tutorials/t1.ts
@@ -40,10 +40,10 @@ console.log(tax, requestStatus);
 const books = ['1984', 'Brave New World', 'Fahrenheit 451'];
 let foundBook: string | undefined;
 for (const book of books) {
-  if (book === '1984') {
-    foundBook = book;
-    break;
-  }
+    if (book === '1984') {
+        foundBook = book;
+        break;
+    }
 }
 console.log(foundBook?.length);
 
@@ -55,7 +55,7 @@ let fruits: string[] = ['apple', 'banana', 'orange'];
 
 
 
-let array: (string | boolean)[] = ['bobo',true, 'orange', false];
+let array: (string | boolean)[] = ['bobo', true, 'orange', false];
 
 console.log(prices, fruits, array);
 
@@ -69,9 +69,9 @@ car.brand = 'ford';
 
 let car1: { brand: string; year: number } = { brand: 'audi', year: 2021 };
 
-let car2: { brand: string; year: number } = { brand: 'audi' };
+//let car2: { brand: string; year: number } = { brand: 'audi' };
 
-console.log(car, car1);
+console.log(car1);
 
 
 let book = { title: 'book', cost: 20 };
@@ -79,6 +79,91 @@ let pen = { title: 'pen', cost: 5 };
 let notebook = { title: 'notebook' };
 
 let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
-items[0].title = 'new book'; // Error: Cannot assign to 'title' because it is a read-only proper
+// items[0].title = 'new book'; // Error: Cannot assign to 'title' because it is a read-only proper
 
 console.log(items);
+
+/* */
+
+function sayHi(name: string) {
+    console.log(`Hello there ${name.toUpperCase()}!!!`);
+}
+sayHi('john');
+// sayHi(3)
+// sayHi('peter', 'random');
+function calculateDiscount(price: number): number {
+    // price.toUpperCase();
+    const hasDiscount = true;
+    if (hasDiscount) {
+        return price;
+        // return 'Discount Applied';
+    }
+    return price * 0.9;
+}
+const finalPrice = calculateDiscount(200);
+console.log(finalPrice);
+// "any" example
+function addThree(number: any) {
+    let anotherNumber: number = 3;
+    return number + anotherNumber;
+}
+const addThreeResult = addThree(2);
+const someValue = addThreeResult;
+
+console.log(someValue)
+// run time error
+//someValue.myMethod();
+
+/* */
+// Function = Optional and Default Parameters
+
+function calculatePrice(price: number, discount?: number) {
+    return price - (discount || 0);
+}
+let priceAfterDiscount = calculatePrice(100, 20);
+console.log(priceAfterDiscount); // Output: 80
+let priceWithoutDiscount = calculatePrice(300);
+console.log(priceWithoutDiscount); // Output: 300
+function calculateScore(initialScore: number, penaltyPoints: number = 0) {
+    return initialScore - penaltyPoints;
+}
+let scoreAfterPenalty = calculateScore(100, 20);
+console.log(scoreAfterPenalty); // Output: 80
+let scoreWithoutPenalty = calculateScore(300);
+console.log(scoreWithoutPenalty); // Output: 300
+
+/* */
+
+function sum(message: string, ...numbers: number[]): string {
+    const doubled = numbers.map((num) => num * 2);
+    console.log(doubled);
+    let total = numbers.reduce((previous, current) => {
+        return previous + current;
+    }, 0);
+    return `${message} ${total}`;
+}
+
+let sumResult = sum('The total is:', 1, 2, 3, 4, 5); // sumResult will be "The total is: 15"
+console.log(sumResult);
+
+/* */
+// Functions - Using Object as Function
+
+function createEmployee({ id }: { id: number }): {
+    id: number;
+    isActive: boolean;
+} {
+    return { id, isActive: id % 2 === 0 };
+}
+const first = createEmployee({ id: 1 });
+const second = createEmployee({ id: 2 });
+console.log(first, second);
+// alternative
+function createStudent(student: { id: number; name: string }) {
+    console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+}
+const newStudent = {
+    id: 5,
+    name: 'anna',
+};
+createStudent(newStudent);
